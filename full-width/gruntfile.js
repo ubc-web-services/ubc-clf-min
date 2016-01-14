3a343a07ba2db033287724fc16bea5620dbd801c
James <james.otoole@ubc.ca>
2016-01-13 17:17:37 -0800

    Update to filename structure and bug fix for https://github.com/ubc-web-services/ubc-clf-min/issues/1

--- full-width/gruntfile.js
+++ full-width/gruntfile.js
@@ -5,16 +5,18 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uncss: {
       options: {
-        report: 'gzip'
+        report: 'gzip',
+        // ignore both of these selectors so neither is stripped by uncss regardless of which is in the markup
+        ignore: ['#ubc7-okanagan-campus', '#ubc7-vancouver-campus']
       },
       dist: {
         files: {
           // issue with uncss right now - https://github.com/addyosmani/grunt-uncss/issues/171
           // workaround - just comment all but one line, then run 'grunt', rinse, repeat
-          //'uncompressed/css/minimal-clf-7.0.4.css': ['sample.html'],
-          //'uncompressed/css/minimal-clf-7.0.4-bw.css': ['sample-bw.html']//,
-          //'uncompressed/css/minimal-clf-7.0.4-gw.css': ['sample-gw.html']//,
-          'uncompressed/css/minimal-clf-7.0.4-wg.css': ['sample-wg.html']
+          'uncompressed/css/minimal-clf-7.0.4.css': ['build.html']//,
+          //'uncompressed/css/minimal-clf-7.0.4-bw.css': ['build-bw.html']//,
+          //'uncompressed/css/minimal-clf-7.0.4-gw.css': ['build-gw.html']//,
+          //'uncompressed/css/minimal-clf-7.0.4-wg.css': ['build-wg.html']
         }
       }
     },
@@ -24,10 +26,10 @@ module.exports = function(grunt) {
       },
       target: {
         files: {
-          'release/css/minimal-clf-7.0.4.css': ['uncompressed/css/minimal-clf-7.0.4.css'],
-          'release/css/minimal-clf-7.0.4-bw.css': ['uncompressed/css/minimal-clf-7.0.4-bw.css'],
-          'release/css/minimal-clf-7.0.4-gw.css': ['uncompressed/css/minimal-clf-7.0.4-gw.css'],
-          'release/css/minimal-clf-7.0.4-wg.css': ['uncompressed/css/minimal-clf-7.0.4-wg.css']
+          'release/css/minimal-clf-7.0.4.css': ['uncompressed/css/minimal-clf-7.0.4.css']//,
+          //'release/css/minimal-clf-7.0.4-bw.css': ['uncompressed/css/minimal-clf-7.0.4-bw.css']//,
+          //'release/css/minimal-clf-7.0.4-gw.css': ['uncompressed/css/minimal-clf-7.0.4-gw.css']//,
+          //'release/css/minimal-clf-7.0.4-wg.css': ['uncompressed/css/minimal-clf-7.0.4-wg.css']
         }
       }
     }
